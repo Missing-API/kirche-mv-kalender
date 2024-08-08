9037e2aa0af9cf55fcd83f682f979a316447c2b3
Jan-Henrik Hempel <jan@schafe-vorm-fenster.de>
2024-08-08 18:14:46 +0200

    Add local timezone in ical format as const.

--- src/constants.ts
+++ src/constants.ts
@@ -4,3 +4,23 @@ export const organizerName: string =
   "Evangelische Kirche in Mecklenburg-Vorpommern";
 export const organizerEmail: string = "info@kirche-mv.de";
 export const organizerEventIdPrefix: string = "kirchemv-";
+
+export const VTIMEZONE = `X-WR-TIMEZONE:Europe/Berlin
+BEGIN:VTIMEZONE
+TZID:Europe/Berlin
+X-LIC-LOCATION:Europe/Berlin
+BEGIN:DAYLIGHT
+TZOFFSETFROM:+0100
+TZOFFSETTO:+0200
+TZNAME:GMT+2
+DTSTART:19700329T020000
+RRULE:FREQ=YEARLY;BYMONTH=3;BYDAY=-1SU
+END:DAYLIGHT
+BEGIN:STANDARD
+TZOFFSETFROM:+0200
+TZOFFSETTO:+0100
+TZNAME:GMT+1
+DTSTART:19701025T030000
+RRULE:FREQ=YEARLY;BYMONTH=10;BYDAY=-1SU
+END:STANDARD
+END:VTIMEZONE`;
